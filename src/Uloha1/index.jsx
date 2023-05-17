d557742a132d6df80e8fe8b21162f31ab53d8c5b
Iva Havranová <havranova.i@seznam.cz>
2023-05-17 18:27:55 +0200

    Uloha1 done

--- src/Uloha1/index.jsx
+++ src/Uloha1/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 // Zadání: Po kliku na tlačítko zobraz bublinu s textem „Ahoj“.
 
 export const Uloha1 = () => {
-	return <button>Ukaž bublinu</button>
-}
+  const showBubble = () => alert('Ahoj');
+  return <button onClick={showBubble}>Ukaž bublinu</button>;
+};
